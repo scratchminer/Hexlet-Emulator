0ebed7a1da5f8ea218c503c43c92793d18ee0e80
scratchminer <scratchminer@gmail.com>
2023-04-17 12:56:14 -0400

    Fix WRAM size (thanks again, Spweesh!)

--- src/memory.h
+++ src/memory.h
@@ -13,7 +13,7 @@ typedef struct {
 	u16 data;
 } mem_Bus;
 
-#define mem_WRAM_SIZE 16384
+#define mem_WRAM_SIZE 32768
 #define mem_VRAM_SIZE 32768
 #define mem_TMRAM_SIZE 4096
 #define mem_HRAM_SIZE 3072
